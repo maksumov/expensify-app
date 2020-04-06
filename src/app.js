d27e45e9e98b7982d133d63b4eb645183fadc480
Marat Maksumov <maxumov@gmail.com>
2020-04-07 01:59:27 +0300

    Setting up budget app & make some routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is a boilerplate</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+  <div>This is from my ExpenseDashboardPage</div>
+);
+const AddExpensePage = () => <div>This is from my AddExpensePage</div>;
+const EditExpensePage = () => <div>This is from my EditExpensePage</div>;
+const HelpPage = () => <div>This is from my HelpPage</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,24 @@ module.exports = {
   entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
-    filename: "app.js"
+    filename: "app.js",
   },
   module: {
     rules: [
       {
         loader: "babel-loader",
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   devtool: "eval-cheap-module-source-map",
   devServer: {
-    contentBase: path.join(__dirname, "public")
-  }
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true,
+  },
 };
