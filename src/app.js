cbde44f0049f5128834d3ec183e98975c0ae7739
Marat Maksumov <maxumov@gmail.com>
2020-04-07 02:38:24 +0300

    Setup the navigation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, NavLink, Route, Switch } from "react-router-dom";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -11,17 +11,47 @@ const ExpenseDashboardPage = () => (
 const AddExpensePage = () => <div>This is from my AddExpensePage</div>;
 const EditExpensePage = () => <div>This is from my EditExpensePage</div>;
 const HelpPage = () => <div>This is from my HelpPage</div>;
-const NotFoundPage = () => <div>404!</div>;
+const NotFoundPage = () => (
+  <div>
+    404! <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <div>
+      <NavLink exact to="/" activeClassName="is-active">
+        Go home
+      </NavLink>{" "}
+      |{" "}
+      <NavLink to="/create" activeClassName="is-active">
+        Add Expense
+      </NavLink>{" "}
+      |{" "}
+      <NavLink to="/edit" activeClassName="is-active">
+        Edit Expense
+      </NavLink>{" "}
+      |{" "}
+      <NavLink to="/help" activeClassName="is-active">
+        Help
+      </NavLink>
+    </div>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
