d0d0f2afda0c83300c1b4346ffa20143617b5196
Marat Maksumov <maxumov@gmail.com>
2020-04-10 03:37:17 +0300

    Create playground with multiple reducers

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers } from "redux";
+
+const demoState = {
+  expenses: [
+    {
+      id: "dsfsdfsdf",
+      description: "January rent",
+      note: "This was the final payment for that address",
+      amount: 54500,
+      createdAt: 0,
+    },
+  ],
+  filters: {
+    text: "rent",
+    sortBy: "amount", // date or amount
+    startDate: undefined,
+    endDate: undefined,
+  },
+};
