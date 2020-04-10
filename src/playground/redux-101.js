a16371331aab0928486a554cf07253e24facbf6d
Marat Maksumov <maxumov@gmail.com>
2020-04-10 03:33:08 +0300

    Separate reducer

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -22,32 +22,34 @@ const resetCount = () => ({
   type: "RESET",
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1. Reducers are pure functions
+// 2. Reducers never changes state or actions
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
       return {
         count: state.count + action.incrementBy,
       };
-
     case "DECREMENT":
       return {
         count: state.count - action.decrementBy,
       };
-
     case "SET":
       return {
         count: action.count,
       };
-
     case "RESET":
       return {
         count: 0,
       };
-
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 // Subscribing to store
 const unsubscribe = store.subscribe(() => {
