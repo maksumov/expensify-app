d7a621412f5c8e019847499228645091bc0686b1
Marat Maksumov <maxumov@gmail.com>
2020-04-14 18:47:20 +0300

    Impement remove ExpenseListItem (my solution)

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { removeExpense } from '../actions/expenses'
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
     <h3>{description}</h3>
     <p>
       {amount} - {createdAt}
     </p>
+    <button
+      onClick={(e) => {
+        dispatch(removeExpense({ id }))
+      }}
+    >
+      Remove
+    </button>
   </div>
 )
 
-export default ExpenseListItem
+const mapStateToProps = (state) => ({
+  expenses: state.expenses,
+})
+
+export default connect(mapStateToProps)(ExpenseListItem)
