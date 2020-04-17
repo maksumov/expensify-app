7dd8e387e5894625ca1ed90853cf65f173fd9604
Marat Maksumov <maxumov@gmail.com>
2020-04-18 00:58:25 +0300

    Switch page after adding expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -9,6 +9,7 @@ const AddExpensePage = (props) => (
     <ExpenseForm
       onSubmit={(expense) => {
         props.dispatch(addExpense(expense))
+        props.history.push('/')
       }}
     />
   </div>
