4704a06b9e3b415b5cdeae564d0d3deeadcb254b
Marat Maksumov <maxumov@gmail.com>
2020-04-18 01:55:36 +0300

    Setup links to edit expenses

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { removeExpense } from '../actions/expenses'
 
 const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
       {amount} - {createdAt}
     </p>
