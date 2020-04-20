58cd5a8598c4f7746560ead6da4ed44a37fbb6f3
Marat Maksumov <maxumov@gmail.com>
2020-04-20 04:03:44 +0300

    Get rid of duplicate code

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,17 +3,19 @@ import { shallow } from 'enzyme'
 import { AddExpensePage } from '../../components/AddExpensePage'
 import expenses from '../fixtures/expenses'
 
+let onSubmit, history, wrapper
+
+beforeEach(() => {
+  onSubmit = jest.fn()
+  history = { push: jest.fn() }
+  wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+})
+
 test('should render AddExpensePage correctly', () => {
-  const onSubmit = jest.fn()
-  const history = { push: jest.fn() }
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
   expect(wrapper).toMatchSnapshot()
 })
 
 test('should handle a submit', () => {
-  const onSubmit = jest.fn()
-  const history = { push: jest.fn() }
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
   expect(history.push).toHaveBeenLastCalledWith('/')
   expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
