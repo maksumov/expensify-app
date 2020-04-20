5a42892619a49ea110dd6b18bf34da0bd684adad
Marat Maksumov <maxumov@gmail.com>
2020-04-20 05:47:46 +0300

    Add tests for EditExpensePage component

---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { EditExpensePage } from '../../components/EditExpensePage'
+import expenses from '../fixtures/expenses'
+
+let wrapper, history, editExpense, removeExpense
+
+beforeEach(() => {
+  history = { push: jest.fn() }
+  editExpense = jest.fn()
+  removeExpense = jest.fn()
+  wrapper = shallow(
+    <EditExpensePage expense={expenses[1]} editExpense={editExpense} history={history} removeExpense={removeExpense} />
+  )
+})
+
+test('should render EditExpensePage', () => {
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('should handle editExpense', () => {
+  const updates = { note: 'My new note value' }
+  wrapper.find('ExpenseForm').prop('onSubmit')(updates)
+  expect(history.push).toHaveBeenLastCalledWith('/')
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[1].id, updates)
+})
+
+test('should handle removeExpense', () => {
+  const id = expenses[1].id
+  wrapper.find('button').prop('onClick')()
+  expect(history.push).toHaveBeenLastCalledWith('/')
+  expect(removeExpense).toHaveBeenLastCalledWith({ id })
+})
