a6b8ed64aae9b4bbbdb9d56db43af004012681dd
Marat Maksumov <maxumov@gmail.com>
2020-04-29 05:47:25 +0300

    Add firebase functions for arrays (playground)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,16 +14,72 @@ firebase.initializeApp(firebaseConfig)
 
 const database = firebase.database()
 
-database
-  .ref()
-  .once('value')
-  .then((snapshot) => {
-    const { name, job } = snapshot.val()
-    console.log(`${name} is a ${job.title} at ${job.company}`)
-  })
-  .catch((e) => {
-    console.log('Error fetching data', e)
-  })
+// child_removed
+database.ref('expenses').on('child_removed', (snapshot) => {
+  console.log('Removed: ', snapshot.key, snapshot.val())
+})
+
+// child_changed
+database.ref('expenses').on('child_changed', (snapshot) => {
+  console.log('Changed: ', snapshot.key, snapshot.val())
+})
+
+// child_added
+database.ref('expenses').on('child_added', (snapshot) => {
+  console.log('Added: ', snapshot.key, snapshot.val())
+})
+
+// database
+//   .ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const data = []
+//     snapshot.forEach((childSnapshot) => {
+//       data.push({ id: childSnapshot.key, ...childSnapshot.val() })
+//     })
+//     console.log(data)
+//   })
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const data = []
+//   snapshot.forEach((childSnapshot) => {
+//     data.push({ id: childSnapshot.key, ...childSnapshot.val() })
+//   })
+//   console.log(data)
+// })
+
+// const expenses = [
+//   {
+//     description: 'Rent',
+//     note: '',
+//     amount: 12234,
+//     createdAt: 1221,
+//   },
+//   {
+//     description: 'Coffee',
+//     note: '',
+//     amount: 567,
+//     createdAt: 2221,
+//   },
+//   {
+//     description: 'Tea',
+//     note: '',
+//     amount: 123,
+//     createdAt: 121,
+//   },
+// ]
+// expenses.forEach((expense) => database.ref('expenses').push(expense))
+
+// database
+//   .ref()
+//   .once('value')
+//   .then((snapshot) => {
+//     const { name, job } = snapshot.val()
+//     console.log(`${name} is a ${job.title} at ${job.company}`)
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e)
+//   })
 
 // database
 //   .ref()
