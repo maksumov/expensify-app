a972ce390508da51055f2d84d356ba919d23f39e
Marat Maksumov <maxumov@gmail.com>
2020-05-18 00:56:26 +0300

    Add authReducer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { startSetExpenses } from './actions/expenses'
+import { login, logout } from './actions/auth'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
@@ -27,6 +28,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
+    store.dispatch(login(user.uid))
     store.dispatch(startSetExpenses()).then(() => {
       renderApp()
       if (history.location.pathname === '/') {
@@ -34,6 +36,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     })
   } else {
+    store.dispatch(logout())
     renderApp()
     history.push('/')
   }
